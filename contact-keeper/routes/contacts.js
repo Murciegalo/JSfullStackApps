7c765ddbe156d99fa968681611ac9bcaf49f407b
Snash <raulmv01@hotmail.es>
2019-11-25 19:13:40 +0000

    Contacts Model & route completed

--- contact-keeper/routes/contacts.js
+++ contact-keeper/routes/contacts.js
@@ -1,26 +1,52 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { check , validationResult } = require('express-validator');
 
-// @route     Get api/contacts
-// Get all user contacts  // Private access
-router.get( '/' , ( req , res ) => {
-  res.send('Get all contacts')
-});
+const User = require('../models/User');
+const Contact = require('../models/Contact');
 
-// @route     Post api/users
-// Add Users   // Private access
-router.post( '/' , ( req , res ) => {
+
+//  @route                   Get api/contacts
+//  Get all user contacts    Private access
+router.get( 
+  '/' , 
+  auth , 
+  async ( req , res ) => {
+    try {
+      const contacts = await Contact.find({ user: req.user.id }).sort({ date: -1 });
+      res.json(contacts);
+    } 
+    catch (error) {
+      console.log(error.message);
+      res.status(500).send({ msg: 'Server error..'});
+    }
+  }
+);
+
+
+
+
+//  @route          Post api/users
+//  Add Users      Private access
+router.post( '/' , auth , ( req , res ) => {
   res.send('Add new contact')
 });
 
-// @route     Update api/users
-// Update users in storage  
+
+
+
+//  @route        Update api/users
+//  Update users in storage  
 router.put( '/:id' , ( req , res ) => {
   res.send('Update contact ')
 });
 
-// @route     Delete api/users
-// Delete users in storage 
+
+
+
+//   @route       Delete api/users
+//   Delete users in storage 
 router.delete( '/:id' , ( req , res ) => {
   res.send('delete contact')
 });
