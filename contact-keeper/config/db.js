33e0246be53ea6ce319e99fc63fa16dd3989eaf7
Snash <raulmv01@hotmail.es>
2019-11-26 19:34:09 +0000

    Edit User contacts & Delete routes completed

--- contact-keeper/config/db.js
+++ contact-keeper/config/db.js
@@ -2,13 +2,15 @@
 const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get("mongoURI");
+mongoose.set('useFindAndModify', false);
 
 const connectDB = async () => {
   try {
     await mongoose.connect( db , {
       useCreateIndex: true,
       useNewUrlParser: true ,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
+      // useFindAndModify: false
     });
     console.log('Mongo DB connected')
   } 
--- contact-keeper/routes/contacts.js
+++ contact-keeper/routes/contacts.js
@@ -29,29 +29,103 @@ router.get(
 
 //  @route          Post api/users
 //  Add Users      Private access
-router.post( '/' , auth , ( req , res ) => {
-  res.send('Add new contact')
+router.post( 
+  '/' , 
+  [ auth ,
+    [  
+      check( 'name' , 'Please , input the name' ).not().isEmpty()
+    ] 
+  ] , 
+  async ( req , res ) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name , email , phone , type  } = req.body;
+
+    try {
+      const nueContact = new Contact({
+        name,
+        email,
+        phone,
+        type , 
+        user: req.user.id
+      });
+      
+      const contact = await nueContact.save();
+      res.json(contact);
+    } 
+    catch (error) {
+      console.log(error.message)
+      res.status(500).send('Server error');
+    }
 });
 
 
 
 
 //  @route        Update api/users
-//  Update users in storage  
-router.put( '/:id' , ( req , res ) => {
-  res.send('Update contact ')
+//  Update users in storage   Private 
+router.put( 
+  '/:id' , 
+  auth, 
+  async ( req , res ) => {
+    const { name , email , phone , type } = req.body;
+
+    //Update Obj Edited Contact
+    const contactFields = {};
+    name ? contactFields.name = name :
+    email? contactFields.email = email : 
+    phone? contactFields.phone = phone :
+    type? contactFields.type = type: '';
+
+    try {
+      let contact = await Contact.findById(req.params.id);
+
+      if(!contact) return res.status(404).json({ msg: "Not found" });
+
+      //User own this contact? 
+      if(contact.user.toString() !== req.user.id){
+        return res.status(401).json({ msg: 'Not authorized'});
+      } 
+        
+      contact = await Contact.findByIdAndUpdate(req.params.id , { $set: contactFields } , { new: true });
+
+      res.json(contact);
+    } 
+    catch (error) {
+      console.log(error.message);
+      res.status(500).json({ msg: "Server Error.."});
+    }
 });
 
 
 
 
 //   @route       Delete api/users
-//   Delete users in storage 
-router.delete( '/:id' , ( req , res ) => {
-  res.send('delete contact')
+//   Delete users in storage   Private
+router.delete( 
+  '/:id' , 
+  auth,  
+  async ( req , res ) => {
+    try {
+      let contact = await Contact.findById(req.params.id);
+      
+      if(!contact) return res.status(404).json({ msg: "Not found" });
+
+      //User own this contact? 
+      if(contact.user.toString() !== req.user.id){
+        return res.status(401).json({ msg: 'Not authorized'});
+      } 
+        
+      await Contact.findByIdAndRemove(req.params.id);
+      res.json({ msg: "Contact Removed"});
+    } 
+    catch (error) {
+      console.log(error.message);
+      res.status(500).json({ msg: "Server Error.."});
+    }
 });
-
-
 
 
 module.exports = router;
