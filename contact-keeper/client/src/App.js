cc82ac5a7fc8afea80dbc622e73cfab646fbd75c
Snash <raulmv01@hotmail.es>
2019-11-27 17:22:50 +0000

    Ui components & styles added

--- contact-keeper/client/src/App.js
+++ contact-keeper/client/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+//router
+import { Switch , Route } from 'react-router-dom';
+//Comps.
+import Nav from './components/layouts/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+//Styles
 import './App.css';
+
 
 const App = () => {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Nav />
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+        </Switch>
+      </div>
+    </>
   );
 }
 
---
+++ contact-keeper/client/src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <>
+      <h1>About this app</h1>
+      <p className="my-1">FullStack react app bro!</p>
+      <p className="bg-dark p">
+        <strong>Version: </strong> 1.0.0
+      </p>
+    </>
+  )
+}
+
+
+
+
+export default About;
